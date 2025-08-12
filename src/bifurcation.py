99ee0dc5a9ff5e1fe21b3cfba37ddf6257bee4e7
Athareh Kollolemad <athareh.k.8203@gmail.com>
2025-08-12 11:04:11 +0000

    add file

---
+++ src/bifurcation.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+p = np.linspace(0.7, 4, 100000)
+m = 0.7
+x = []
+y = []
+for u in p:
+    x.append(u)
+    m = np.random.random()
+    for n in range(1001):
+        m = (u*m)*(1-m)
+    for i in range(1051):
+        m = (u*m)*(1-m)
+    y.append(m)
+plt.plot(x, y, ls=' ', marker=',')
+plt.xlabel('X')
+plt.ylabel("alfa")
+plt.savefig("./src/bifurcation.png", dpi=300)
+plt.show()
